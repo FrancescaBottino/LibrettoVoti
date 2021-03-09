7f167f546dc0e68d7fc429ee1264ff22024e72fa
frensis <frensis@192.168.1.62>
2021-03-09 09:37:53 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovotiModel/Libretto.java
+++ src/main/java/it/polito/tdp/librettovotiModel/Libretto.java
@@ -20,6 +20,55 @@ public class Libretto {
 		
 	}
 	
+	/*public void stampaVotiUguali(int punteggio) {
+		//il libretto stampa solo i voti, non ha i riferimenti agli 
+		//oggetti, non posso fare altre operazioni
+	}
+	
+	//Meglio questa seconda
+	
+	public String votiUguali(int punteggio) {
+		///calcola una strnga che contiene i voti e 
+		//sarà poi il chiamante a stamparli
+		//cosa stampo?
+		
+	}
+	*/
+	//ma ancora meglio.. 
+	
+	public List<Voto> listaVotiUguali(int punteggio){
+		
+		//restituisce una lista di voti uguali al criterio
+		//do anche altre informazioni poi chi li vuole li usa/stampa
+		//Difetto : espone all'esterno il tipo di struttura dati che ho usato
+		
+		List<Voto> risultato= new ArrayList<>();
+		
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);  //v è contemporaneamente contenuto in 2
+			                       //liste diverse: v è lo stesso oggetto 
+			                       
+		}
+		
+		return risultato;
+		
+		
+	}
+	
+	public Libretto votiUguali(int punteggio) {
+		//meglio di tutte, retituisco un libretto
+	
+		Libretto risultato= new Libretto();
+		
+		for(Voto v: this.voti) {
+		if(v.getVoto()==punteggio)
+			risultato.add(v); //o anche libretto.voti.add(v)
+		}
+		return risultato;
+		
+	}
+	
 	public String toString() {
 		String s="";
 		for (Voto v: this.voti) {
--- src/main/java/it/polito/tdp/librettovotiModel/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovotiModel/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovotiModel;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 	
@@ -14,9 +15,17 @@ public class TestLibretto {
 	  libretto.add(voto1);
 	  libretto.add(new Voto("Fisica 1", 25, LocalDate.of(2021,  1, 18)));
 	  libretto.add(new Voto("Informatica", 23, LocalDate.of(2019,  2, 1)));
-	
+	  
+	  
 	  
 	  System.out.println(libretto);
+	  
+	  List<Voto> venticinque=libretto.listaVotiUguali(25);
+	  System.out.println(venticinque); //è una arraylist il risultato, cambia la stampa
+	  
+	  Libretto librettoVenticinque= libretto.votiUguali(25);
+	  System.out.println(librettoVenticinque); //è un oggetto diverso, cambia stampa e funzionalità
+	  
 	  
 	}
 	
